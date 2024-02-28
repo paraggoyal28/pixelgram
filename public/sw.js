c2d1ac78e3f069259ecdd42e2cc6c43801c9dd9d
Parag Kumar Goyal <1997parag.goyal@gmail.com>
2024-02-28 22:05:49 +0530

    update service worker to add static files and fetched files from network in cache

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,21 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('static').then(cache => {
       console.log('[Service Worker] PreCaching App shell')
-      cache.add('/src/js/app.js')
+      cache.addAll([
+        '/src/js/app.js',
+        '/index.html',
+        '/',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+        'https://fonts.googleapis.com/icon?family=Material+Icons'
+      ])
     })
   )
 })
@@ -20,7 +34,12 @@ self.addEventListener('fetch', event => {
       if (response) {
         return response
       } else {
-        return fetch(event.request)
+        return fetch(event.request).then(res => {
+          return caches.open('dynamic').then(cache => {
+            cache.put(event.request.url, res.clone())
+            return res
+          })
+        })
       }
     })
   )
